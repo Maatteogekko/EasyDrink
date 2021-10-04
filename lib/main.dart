94b754ee883cb41a6f4b665d4e61003c9df01a19
Maatteogekko <matteogent9991@gmail.com>
2021-10-04 20:56:19 +0200

    removed test instructions

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 
-import 'cocktail/infrastructure/cocktail_repository.dart';
 import 'core/presentation/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // TODO remove in production
   await Wakelock.enable();
-
-  // TODO remove in production
-  final repo = CocktailRepository();
-  final list = await repo.getCocktailsByName("margarita");
-  for (var item in list) {
-    print(item.name);
-  }
 
   runApp(AppWidget());
 }
