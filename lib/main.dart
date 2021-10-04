3e8ef55bebdea7dddf253241f3b07eecfb73134e
Maatteogekko <matteogent9991@gmail.com>
2021-10-04 11:44:48 +0200

    small fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:easy_drink/home/infrastructure/cocktail_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 
+import 'cocktail/infrastructure/cocktail_repository.dart';
 import 'core/presentation/app_widget.dart';
 
 Future<void> main() async {
@@ -11,8 +11,8 @@ Future<void> main() async {
 
   // TODO remove in production
   final repo = CocktailRepository();
-  final list = await repo.getCocktailByName("margarita");
-  for (var item in list!) {
+  final list = await repo.getCocktailsByName("margarita");
+  for (var item in list) {
     print(item.name);
   }
 
