d9a1e5ca3b53f99fee6d23813c760ad0c608ce1c
Maatteogekko <matteogent9991@gmail.com>
2021-10-04 22:16:19 +0200

    added favorites and ingredient methods

--- lib/cocktail/application/cocktails_notifier.dart
+++ lib/cocktail/application/cocktails_notifier.dart
@@ -11,15 +11,41 @@ class CocktailsNotifier extends ChangeNotifier {
   List<Cocktail> _cocktails = [];
   UnmodifiableListView<Cocktail> get cocktails => UnmodifiableListView(_cocktails);
 
+  final List<Cocktail> _favoriteCocktails = [];
+  UnmodifiableListView<Cocktail> get favoriteCocktails => UnmodifiableListView(_favoriteCocktails);
+
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
-  Future searchCocktail(String name) async {
+  Future searchCocktailByName(String name) async {
     _isLoading = true;
     notifyListeners();
 
     // TODO manage exception
     _cocktails = await _repository.getCocktailsByName(name);
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void addFavorite(Cocktail cocktail) {
+    if (_favoriteCocktails.contains(cocktail)) {
+      return;
+    }
+
+    _favoriteCocktails.add(cocktail);
+  }
+
+  void removeFavorite(Cocktail cocktail) => _favoriteCocktails.remove(cocktail);
+
+  void searchCocktailByIngredient(String ingredient) async {
+    _isLoading = true;
+    notifyListeners();
+
+    // TODO manage exception
+    _cocktails = await _repository.getCocktailsByIngredient(ingredient);
+
+    _isLoading = false;
     notifyListeners();
   }
 }
