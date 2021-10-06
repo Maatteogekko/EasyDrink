b852317e52478785f7dcfacac7ec15d5cfe989d1
Maatteogekko <matteogent9991@gmail.com>
2021-10-06 13:25:12 +0200

    better back button behavior on main_view

--- lib/main_view/core/presentation/main_view_page.dart
+++ lib/main_view/core/presentation/main_view_page.dart
@@ -29,37 +29,49 @@ class MainViewPage extends StatelessWidget {
           );
         },
         bottomNavigationBuilder: (context, tabsRouter) {
-          return SalomonBottomBar(
-            currentIndex: tabsRouter.activeIndex,
-            onTap: (index) {
-              tabsRouter.setActiveIndex(index);
+          void setActiveIndex(int index) {
+            tabsRouter.setActiveIndex(index);
 
-              SystemChrome.setSystemUIOverlayStyle(
-                SystemUiOverlayStyle(
-                  statusBarColor: _colors[index]?.withOpacity(0.8),
-                ),
-              );
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle(
+                statusBarColor: _colors[index]?.withOpacity(0.8),
+              ),
+            );
+          }
+
+          return WillPopScope(
+            onWillPop: () async {
+              if (tabsRouter.activeIndex != 0) {
+                setActiveIndex(0);
+                return false;
+              } else {
+                return true;
+              }
             },
-            items: [
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.home_outlined),
-                title: const Text("Home"),
-                activeIcon: const Icon(Icons.home),
-                selectedColor: Theme.of(context).primaryColor,
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.star_border),
-                title: const Text("Preferiti"),
-                activeIcon: const Icon(Icons.star),
-                selectedColor: Colors.amber,
-              ),
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.settings_outlined),
-                title: const Text("Impostazioni"),
-                activeIcon: const Icon(Icons.settings),
-                selectedColor: Colors.purple,
-              )
-            ],
+            child: SalomonBottomBar(
+              currentIndex: tabsRouter.activeIndex,
+              onTap: setActiveIndex,
+              items: [
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.home_outlined),
+                  title: const Text("Home"),
+                  activeIcon: const Icon(Icons.home),
+                  selectedColor: Theme.of(context).primaryColor,
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.star_border),
+                  title: const Text("Preferiti"),
+                  activeIcon: const Icon(Icons.star),
+                  selectedColor: Colors.amber,
+                ),
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.settings_outlined),
+                  title: const Text("Impostazioni"),
+                  activeIcon: const Icon(Icons.settings),
+                  selectedColor: Colors.purple,
+                )
+              ],
+            ),
           );
         },
       ),
